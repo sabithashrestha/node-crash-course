c0b602cd690b9b8d6e3b6aafcbbdc0e32e3be424
sabithashrestha <sabithashrestha@gmail.com>
2026-09-10 00:23:18 +1000

    EJS,view engines, passing data into views

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 const express = require("express");
+const { create } = require("lodash");
+const { title } = require("node:process");
 
 // express app
 const app = express();
+
+// register view engine
+app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+    },
+    {
+      title: "Mario finds stars",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+    },
+    {
+      title: "How to use AI",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create New Blog" });
 });
 
 // 404 page IMP should stay at bottom
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
