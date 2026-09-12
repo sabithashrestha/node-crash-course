3434eeecb288df9d6a17cca5bed6577e770e73d7
sabithashrestha <sabithashrestha@gmail.com>
2026-09-12 16:29:17 +1000

    MongoDB, mongoose

--- app.js
+++ app.js
@@ -1,39 +1,109 @@
 const express = require("express");
 const { create } = require("lodash");
 const { title } = require("node:process");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
+// connct to MongoDB
+const dbURI =
+  "mongodb+srv://newuser:newuser1234@nodetuts.2lzvyzg.mongodb.net/note-tuts";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
+app.use(morgan("dev"));
+
+// // mongoose and mongo sandbox routes
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "new blog",
+//     snippet: "about my new blog",
+//     body: "more about my new blog",
+//   });
+
+//   blog
+//     .save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("6aa4e32a2a00531abd32685f")
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+app.use((req, res, next) => {
+  // console.log("new ewquest made");
+  // console.log("host", req.hostname);
+  // console.log("path", req.path);
+  // console.log("method", req.method);
+  next();
+});
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
-    },
-    {
-      title: "Mario finds stars",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
-    },
-    {
-      title: "How to use AI",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
+
+  // const blogs = [
+  //   {
+  //     title: "Yoshi finds eggs",
+  //     snippet:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+  //   },
+  //   {
+  //     title: "Mario finds stars",
+  //     snippet:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+  //   },
+  //   {
+  //     title: "How to use AI",
+  //     snippet:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem beatae dignissimos odio. Culpa consequatur voluptatum nihil at. Provident, aliquam. Voluptates, necessitatibus. Vero perferendis, molestiae autem quaerat officia totam quidem.",
+  //   },
+  // ];
+  // res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
